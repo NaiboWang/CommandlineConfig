da543d28ac21e4d2ab79102f9f34dcb9284cf988
naibo <naibowang@foxmail.com>
2024-03-08 17:42:33 +0800

    Add ZSH

---
+++ zsh_install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt install git
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+rm ~/.zshrc
+cp zshrc ~/.zshrc
+zsh
